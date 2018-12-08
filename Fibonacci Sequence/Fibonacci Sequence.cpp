a261d1e46cc29f7010308040ef256298df06b62f
Jonah Alexander-Awad <34581478+JonahAwad94@users.noreply.github.com>
2018-12-07 22:14:54 -0500

    Corrected Notes

--- Fibonacci Sequence/Fibonacci Sequence.cpp
+++ Fibonacci Sequence/Fibonacci Sequence.cpp
@@ -1,12 +1,12 @@
 //////////////////////////////////////////////////////////////////////////////////////
-// Quick Math program 1 - Fibonacci													//
-// Created - 11/5/18 by Jonah Alexander												//
-// Last Modified - 11/7/18 by Jonah Alexander										//
-//																					//
-// NOTES:																			//
-// Can be done via recursion, but will be exponentially more resource intensive		//
+// Quick Math program 1 - Fibonacci                                                 //
+// Created - 11/5/18 by Jonah Alexander                                             //
+// Last Modified - 11/7/18 by Jonah Alexander                                       //
+//                                                                                  //
+// NOTES:                                                                           //
+// Can be done via recursion, but will be exponentially more resource intensive     //
 // We are limiting the max input to 93 to avoid overflow on unsigned long long  int //
-//  - This can be raised by using a user defined data type							//
+//  - This can be raised by using a user defined data type                          //
 //////////////////////////////////////////////////////////////////////////////////////
 #include "pch.h" //For visual studio 2017
 #include <iostream>
