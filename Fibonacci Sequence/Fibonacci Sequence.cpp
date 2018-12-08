61603869a1dfef865f68722f6c7744b446f8bdf7
Jonah Alexander-Awad <34581478+JonahAwad94@users.noreply.github.com>
2018-12-07 22:35:18 -0500

    Moved Documentation to "README.md"

--- Fibonacci Sequence/Fibonacci Sequence.cpp
+++ Fibonacci Sequence/Fibonacci Sequence.cpp
@@ -1,13 +1,5 @@
-//////////////////////////////////////////////////////////////////////////////////////
-// Quick Math program 1 - Fibonacci                                                 //
-// Created - 11/5/18 by Jonah Alexander                                             //
-// Last Modified - 11/7/18 by Jonah Alexander                                       //
-//                                                                                  //
-// NOTES:                                                                           //
-// Can be done via recursion, but will be exponentially more resource intensive     //
-// We are limiting the max input to 93 to avoid overflow on unsigned long long  int //
-//  - This can be raised by using a user defined data type                          //
-//////////////////////////////////////////////////////////////////////////////////////
+// See "README.md"
+
 #include "pch.h" //For visual studio 2017
 #include <iostream>
 
